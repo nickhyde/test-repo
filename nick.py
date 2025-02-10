59474733062e9de46dbe01f8807762df8238f5c1
nickhyde <nickhyde314@yahoo.com>
2025-02-09 19:04:30 -0700

    new detached HEAD

--- nick.py
+++ nick.py
@@ -1,2 +1,3 @@
 import os
-print "Fixed detached head, but now doing a git checkout main without commit to see what happens."
+print("Fixed detached head, but now doing a git checkout main without commit to see what happens.")
+print("now creating a new detached HEAD.")
